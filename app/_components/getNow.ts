8bd5c5552e910a1c23661a8a9ef5f48d909fb258
xiushu53 <xiushu53@gmail.com>
2024-06-20 23:37:20 +0900

    🐛

--- app/_components/getNow.ts
+++ app/_components/getNow.ts
@@ -1,4 +1,4 @@
 export const getNow = async () => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/now`).then(d=>d.json())
+    const res = await fetch(`${process.env.NEXT_PUBLIC_VERCEL_URL}/api/now`).then(d=>d.json())
     return res.now
 }
